d1ee7f06648634ddb8f504c1630f54f73b10e6d1
abdellah karani <abdellahkarani@gamil.com>
2024-10-04 22:57:30 +0100

    .

--- main.js
+++ main.js
@@ -21,46 +21,56 @@ let mode_ele = document.querySelector(".ele_mode");
 
 mode_ele.addEventListener("click", function() {
     if (mode_ele.classList.contains("light")) {
-        mode_ele.classList.add("dark");
-        mode_ele.classList.remove("light");
-        // add some changes to the content page
+        change_icon("dark", "light");
         document.body.style.backgroundColor = "white";
-        
-        const eles_white = document.querySelectorAll(".white");
-        eles_white.forEach(element => {
-            element.classList.remove("white");
-            element.classList.add("dark");
-        });
-        
-        let title_eles = document.querySelectorAll(".title span");
-        title_eles.forEach(element => {
-            element.classList.add("dark");
-            element.style.backgroundColor = "white";
-        });
-
-        let cards = document.querySelectorAll(".card");
-        cards.forEach(element => {
-            element.classList.add("light");
-        });
-
-        let blue_eles = document.querySelectorAll(".blue");
-        blue_eles.forEach(element => {
-            element.classList.add("blue-light");
-            element.classList.remove("blue");
-        });
-
-        let nav_links_eles = document.querySelectorAll(".nav-links a");
-        nav_links_eles.forEach(element => {
-            element.classList.add("blue-light");
-            element.classList.remove("blue");
-        });
-
+        set_titles_classes("white", "dark");
+        set_blue_classes("blue", "blue-light");
+        set_white_classes("white", "dark");
     }
     else {
-        mode_ele.classList.add("light");
-        mode_ele.classList.remove("dark");
+        change_icon("light", "dark");
+        document.body.style.backgroundColor = "#0a192f";
+        set_titles_classes("dark", "white");
+        set_blue_classes("blue-light", "blue");
+        set_white_classes("dark", "white");
     }
 });
+
+
+function change_icon(to_add, to_remove) {
+    mode_ele.classList.add(to_add);
+    mode_ele.classList.remove(to_remove);
+}
+
+
+function set_white_classes(to_add, to_remove) {
+    const eles_white = document.querySelectorAll(`.${to_add}`);
+    eles_white.forEach(element => {
+        element.classList.add(to_remove);
+        element.classList.remove(to_add);
+    });
+}
+
+
+function set_blue_classes(to_add, to_remove) {
+    let blue_eles = document.querySelectorAll(`.${to_add}`);
+    blue_eles.forEach(element => {
+        element.classList.add(to_remove);
+        element.classList.remove(to_add);
+    });
+}
+
+function set_titles_classes(to_add, to_remove) {
+    let title_eles = document.querySelectorAll(".title span");
+    title_eles.forEach(element => {
+        element.classList.add(to_add);
+        element.classList.remove(to_remove);
+    });
+}
+
+
+
+
 
 
 
